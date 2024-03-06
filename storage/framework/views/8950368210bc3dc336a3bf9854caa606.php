4b83e365eed61ed72b7f2a585d6e03ce186e4c22
nvxhc <139727953+nvxhc@users.noreply.github.com>
2024-03-05 23:06:31 -0500

    Add files via upload

---
+++ storage/framework/views/8950368210bc3dc336a3bf9854caa606.php
@@ -0,0 +1,202 @@
+<?php $__env->startSection('title', 'Perfil de usuario'); ?>
+
+
+
+<?php $__env->startSection('content'); ?>
+ <?php $__env->slot('header', null, []); ?> 
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <?php echo e(__('Profile')); ?>
+
+    </h2>
+ <?php $__env->endSlot(); ?>
+
+<div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <?php if(Laravel\Fortify\Features::canUpdateProfileInformation()): ?>
+            <?php
+$__split = function ($name, $params = []) {
+    return [$name, $params];
+};
+[$__name, $__params] = $__split('profile.update-profile-information-form');
+
+$__html = app('livewire')->mount($__name, $__params, 'lw-3237811506-0', $__slots ?? [], get_defined_vars());
+
+echo $__html;
+
+unset($__html);
+unset($__name);
+unset($__params);
+unset($__split);
+if (isset($__slots)) unset($__slots);
+?>
+
+            <?php if (isset($component)) { $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.section-border','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $attributes = $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $component = $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+        <?php endif; ?>
+
+        <?php if(Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords())): ?>
+            <div class="mt-10 sm:mt-0">
+                <?php
+$__split = function ($name, $params = []) {
+    return [$name, $params];
+};
+[$__name, $__params] = $__split('profile.update-password-form');
+
+$__html = app('livewire')->mount($__name, $__params, 'lw-3237811506-1', $__slots ?? [], get_defined_vars());
+
+echo $__html;
+
+unset($__html);
+unset($__name);
+unset($__params);
+unset($__split);
+if (isset($__slots)) unset($__slots);
+?>
+            </div>
+
+            <?php if (isset($component)) { $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.section-border','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $attributes = $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $component = $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+        <?php endif; ?>
+
+        <?php if(Laravel\Fortify\Features::canManageTwoFactorAuthentication()): ?>
+            <div class="mt-10 sm:mt-0">
+                <?php
+$__split = function ($name, $params = []) {
+    return [$name, $params];
+};
+[$__name, $__params] = $__split('profile.two-factor-authentication-form');
+
+$__html = app('livewire')->mount($__name, $__params, 'lw-3237811506-2', $__slots ?? [], get_defined_vars());
+
+echo $__html;
+
+unset($__html);
+unset($__name);
+unset($__params);
+unset($__split);
+if (isset($__slots)) unset($__slots);
+?>
+            </div>
+
+            <?php if (isset($component)) { $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.section-border','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $attributes = $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $component = $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+        <?php endif; ?>
+
+        <div class="mt-10 sm:mt-0">
+            <?php
+$__split = function ($name, $params = []) {
+    return [$name, $params];
+};
+[$__name, $__params] = $__split('profile.logout-other-browser-sessions-form');
+
+$__html = app('livewire')->mount($__name, $__params, 'lw-3237811506-3', $__slots ?? [], get_defined_vars());
+
+echo $__html;
+
+unset($__html);
+unset($__name);
+unset($__params);
+unset($__split);
+if (isset($__slots)) unset($__slots);
+?>
+        </div>
+
+        <?php if(Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures()): ?>
+            <?php if (isset($component)) { $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.section-border','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('section-border'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $attributes = $__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__attributesOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea)): ?>
+<?php $component = $__componentOriginal7b32e2c8c86a088fa824ad9246edeeea; ?>
+<?php unset($__componentOriginal7b32e2c8c86a088fa824ad9246edeeea); ?>
+<?php endif; ?>
+
+            <div class="mt-10 sm:mt-0">
+                <?php
+$__split = function ($name, $params = []) {
+    return [$name, $params];
+};
+[$__name, $__params] = $__split('profile.delete-user-form');
+
+$__html = app('livewire')->mount($__name, $__params, 'lw-3237811506-4', $__slots ?? [], get_defined_vars());
+
+echo $__html;
+
+unset($__html);
+unset($__name);
+unset($__params);
+unset($__split);
+if (isset($__slots)) unset($__slots);
+?>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.dash', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\SistemaFac\resources\views/dashboard/profile.blade.php ENDPATH**/ ?>
