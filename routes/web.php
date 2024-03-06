5afb49409048f755c82be64cf159391eeb5ebffb
nvxhc <139727953+nvxhc@users.noreply.github.com>
2024-03-05 23:22:36 -0500

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('layouts.login');
+})->name('root');
+Route::get('/productos', function () {
+    return view('dashboard.products');
+})->name('products');
+Route::get('/perfil', function () {
+    return view('dashboard.profile');
+})->name('profile');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->name('dashboard');
+});
